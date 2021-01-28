3f4f876dc293d9e8d53d24355229fb5d78b6a689
emada92 <77917294+emada92@users.noreply.github.com>
2021-01-28 20:00:15 +0100

    Update img+bb_coco.py

--- img+bb_coco.py
+++ img+bb_coco.py
@@ -10,24 +10,24 @@ import tensorflow as tf
 def save_random_coco_images(num_of_images, category):
     for i in range(num_of_images):
         dataType='val2017'
-        annFile='C:/Users/emila/annotations/instances_{}.json'.format(dataType);
-        coco=COCO(annFile);    
-        catIds = coco.getCatIds(catNms=[category]);
-        img_id_list = coco.getImgIds(catIds = catIds );
-        img_id = int(np.random.choice(img_id_list));
-        img = coco.loadImgs(img_id)[0];
+        annFile='C:/Users/emila/annotations/instances_{}.json'.format(dataType)
+        coco=COCO(annFile)
+        catIds = coco.getCatIds(catNms=[category])
+        img_id_list = coco.getImgIds(catIds = catIds )
+        img_id = int(np.random.choice(img_id_list))
+        img = coco.loadImgs(img_id)[0]
         
         #draw bouding boxes
-        annIds = coco.getAnnIds(imgIds=img['id']);
-        anns = coco.loadAnns(annIds)[0];
-        bbox = anns['bbox'];
+        annIds = coco.getAnnIds(imgIds=img['id'])
+        anns = coco.loadAnns(annIds)[0]
+        bbox = anns['bbox']
         
-        x, y, w, h = bbox[0], bbox[1], bbox[2], bbox[3];
+        x, y, w, h = bbox[0], bbox[1], bbox[2], bbox[3]
 
-        I = io.imread(img['coco_url']);
+        I = io.imread(img['coco_url'])
         
-        fig, ax = plt.subplots(1);
-        rect = patches.Rectangle((x, y), w, h, fill=False, edgecolor='r');
-        ax.imshow(I);
-        ax.add_patch(rect);
-        plt.savefig(f'mygraph_{category}{i}.png');
+        fig, ax = plt.subplots(1)
+        rect = patches.Rectangle((x, y), w, h, fill=False, edgecolor='r')
+        ax.imshow(I)
+        ax.add_patch(rect)
+        plt.savefig(f'mygraph_{category}{i}.png')
